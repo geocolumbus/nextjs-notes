193f3aeffb26a34a78ff4791d54865a8f9625224
George Campbell <campbelg@oclc.org>
2020-11-03 11:02:20 -0500

    Organize how components will be displayed

---
+++ components/Inputs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+
+const Inputs = () => {
+    return <>
+        <h2>Inputs</h2>
+        <h3>Button</h3>
+        <Button>Default</Button>
+        <Button></Button>
+        <h3>Button Group</h3>
+        <h3>Checkbox</h3>
+    </>
+}
+
+export default Inputs
